159ad6d6e766d26d1b331776db89aad09f488969
Wikan Setiaji <wikansetiaji@gmail.com>
2025-02-28 20:35:33 +0700

    Add: create project intial commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Wikan Setiaji on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      MusicListPage()
+    }
+  }
+}
---
+++ MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by Wikan Setiaji on 28/02/25.
+//
+
+import Testing
+
+struct MusicPlayerTests {
+  
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+  
+}
